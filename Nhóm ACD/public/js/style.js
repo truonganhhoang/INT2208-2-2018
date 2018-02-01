a4026f5cf4340d9a242822186b9272de1de2a809
kedumuc1712 <sieutroma7k51@gmail.com>
2018-02-01 20:58:49 +0700

    Cau truc lai thu muc CSS

--- Nhóm ACD/public/js/style.js
+++ Nhóm ACD/public/js/style.js
@@ -1,35 +1,82 @@
-$(document).ready(function() {
-	$(document).on('click', 'header .sign-in', function(event) {
-		var view_width = $(window).width();
-		if (view_width < 768) {
-			$("header").css("display", "none");
-			$(".intro").css("display", "none");
-			$(".popup-login").css("display", "block");
-		}
-		else {
-			$(".popup-login").addClass('alert-popup');
-		}
-		
-		$('html, body').css({
+$(document).ready(function () {
+    $(document).on('click', 'header .sign-in, header .sign-up', function (event) {
+        var view_width = $(window).width();
+        if (view_width < 768) {
+            $("header").css("display", "none");
+            $(".intro").css("display", "none");
+            if ($(this).hasClass('sign-in')) {
+                $(".popup-login").css("display", "block");
+            }
+            else {
+                $(".popup-signup").css("display", "block");
+            }
+        }
+        else {
+            if ($(this).hasClass('sign-in')) {
+                $(".popup-login").addClass('alert-popup');
+            }
+            else {
+                $(".popup-signup").addClass('alert-popup');
+            }
+        }
+
+        $('html, body').css({
             overflow: 'hidden',
-    		height: '100%'
-		});
-	});
+            height  : '100%'
+        });
+    });
 
-	$(document).on("click", ".cancel", function(event) {
-		$('html, body').css({
+    $(document).on("click", ".cancel", function (event) {
+        $('html, body').css({
             overflow: 'visible'
-		});
+        });
 
-		var view_width = $(window).width();
-		if (view_width < 768) {
-			$("header").css("display", "block");
-			$(".intro").css("display", "block");
-			$(".popup-login").css("display", "none");
-		}
-		else {
-			$(".popup-login").removeClass('alert-popup');
-		}
-		
-	});
+        var view_width = $(window).width();
+        if (view_width < 768) {
+            $("header").css("display", "block");
+            $(".intro").css("display", "block");
+
+            if ($(this).hasClass('cancel-log')) {
+                $(".popup-login").css("display", "none");
+            }
+            else {
+                $(".popup-signup").css("display", "none");
+            }
+        }
+        else {
+            if ($(this).hasClass('cancel-log')) {
+                $(".popup-login").removeClass('alert-popup');
+            }
+            else {
+                $(".popup-signup").removeClass('alert-popup');
+            }
+        }
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop           : true,
+        responsiveClass: true,
+        responsive     : {
+            0   : {
+                items: 2
+            },
+            576 : {
+                items: 4,
+                nav  : false
+            },
+            768 : {
+                items: 4,
+                nav  : false
+            },
+            992 : {
+            	item: 5
+            },
+            1000: {
+                items: 6,
+               /* nav  : true*/
+                loop : false,
+                margin: 20
+            }
+        }
+    })
 });
