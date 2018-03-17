35b8c2a90b4ef29389f2476a50357ab01dbd2d30
Gou-Opa <monneymrt1998@gmail.com>
2018-03-18 02:08:28 +0700

    Nop bai tap tuan 6 (chua day du)

---
+++ NguyenNgocTien/Bai-tap-tuan-6/js/datajs/1.js
@@ -0,0 +1,26 @@
+//alert("1");
+var quest = [
+	[
+		"Lệnh jmp tagname có chức năng gì",
+		"Xóa bộ nhớ ở thanh ghi tagname",
+		"Nhảy đến câu lệnh ở vị trí tagname",
+		"Lệnh sai cú pháp"
+		"Chuyển vị trí thanh ghi đến thanh ghi tagname",
+		"B"
+	],
+	[
+		"Đổi giá trị 987 ra dãy 16 bit",
+		"0000001111011011",
+		"0000111101101100",
+		"0011110110110000",
+		"1111011011000000"
+	],
+	[
+		"Cờ nào cho biết kết quả thanh ghi âm",
+		"CF = 0",
+		"ZF = 1",
+		"OF = 1",
+		"SF = 1",
+		"D"
+	]
+]
