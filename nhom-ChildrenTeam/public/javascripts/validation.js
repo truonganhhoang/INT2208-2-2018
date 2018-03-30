b650a236d499fff6ab770ed6b636e4a5b726f0da
ThuyTienUet <tien280198@gmail.com>
2018-03-30 21:28:57 +0700

    update

---
+++ nhom-ChildrenTeam/public/javascripts/validation.js
@@ -0,0 +1,12 @@
+$('#addReview').submit(function (e){
+	$('.alert.alert-danger').hide();
+	if (!$('input#name').val() || !$('select#rating').val() || !$('textarea#review').val()){
+		if ($('.alert.alert-danger').length){
+			$('.alert.alert-danger').show();
+		}else{
+			$(this).prepend('<div role="alert" class="alert alert-danger">All fields required, please try again</div>');
+		}
+	    return false;
+	}
+});
+//$('addReview').submit(function (e){$('.alert.alert-danger').hide();if(!$('input#name').val()||!$('select#rating').val()||!$('textarea#review').val()){if ($('.alert.alert-danger').length){$('.alert.alert-danger').show();}else{$(this).prepend('<div role="alert" class="alert alert-danger">All fields required, please try again</div>');}return false;}});
