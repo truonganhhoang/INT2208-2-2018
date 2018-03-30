c83d182111b061de4a41fb3388c89dd419df9aa2
ThuyTienUet <tien280198@gmail.com>
2018-03-30 20:03:16 +0700

    nop bai tap

--- PhamThiThuyTien/app/app/luyentap/cal-question.ts
+++
@@ -1,7 +0,0 @@
-export class CalQuestion {
-    id: string;
-    num_1: number;
-    num_2: number;
-    signal: string;
-    ans: number;
-}
--- PhamThiThuyTien/app/app/luyentap/chuong5-thoigian/clock.ts
+++
@@ -1,7 +0,0 @@
-export class Clock{
-    link_img : string;
-    ans_A : string;
-    ans_B: string;
-    ans_C: string;
-    true_ans: string;
-}
