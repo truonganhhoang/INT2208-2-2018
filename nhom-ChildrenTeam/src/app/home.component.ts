8d3d78bd793035ed38fb8d3c43fe150931e07243
ThuyTienUet <tien280198@gmail.com>
2018-04-10 22:27:02 +0700

    vs 1

---
+++ nhom-ChildrenTeam/src/app/home.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'home',
+    templateUrl:'home.component.html',
+    styleUrls: ['home.component.css'],
+})
+
+export class HomeComponent{
+  titles = ["Luyện tập", "Kiểm tra", "Giải trí"]
+}
---
+++ nhom-ChildrenTeam/src/app/luyentap/chuong2-phamvi20/sub-20-question.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cal-sub-20',
+    styleUrls:['template-sub-sum.css'],
+    templateUrl: 'sub-20-question.component.html',
+})
+
+export class Sub20Question {
+    showAns = true;
+    nextLess = false;
+    endLessPer = false;
+    endLess = false;
+    count = 0;
+    a = Math.floor((Math.random() * 20) + 1);
+    b = Math.floor((Math.random() * 20) + 0);
+    sub_1 = Math.max(this.a, this.b);
+    sub_2 = Math.min(this.a, this.b);
+
+    rand() {
+        var num_1 = Math.floor((Math.random() * 20) + 1);
+        var num_2 = Math.floor((Math.random() * 20) + 0);
+        this.sub_1 = Math.max(num_1, num_2);
+        this.sub_2 = Math.min(num_1, num_2);
+    }
+    rightAns(): void {      // trả lời đúng thêm một sao vào khối có id = starAward
+        var img = document.createElement("IMG");
+        img.setAttribute("src", "./assets/image/true.png");
+        document.getElementById("starAward").appendChild(img);
+
+    }
+
+    checkAns(value) {
+        if (value == '') {
+            alert("Bạn phải điền giá trị vào ô trống!");
+        }
+        else if (value != ' ' && parseInt(value) == (this.sub_1 - this.sub_2)) {
+
+            this.rightAns();
+            this.rand();
+            this.count += 1;
+
+            if (this.count == 5) {
+                this.showAns = false;
+                this.nextLess = true;
+                this.endLessPer = true;
+            }
+        } else if (value != '') {
+            this.count += 1;
+            this.rand();
+            if (this.count == 5) {
+                this.showAns = false;
+                this.nextLess = true;
+                this.endLess = true;
+            }
+        }
+    }
+}
+
